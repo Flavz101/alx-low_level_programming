306eeb087c41011d6000fae7b19ef192ee9cc6f0
favour <chrisfavour15@gmail.com>
2022-07-05 11:25:20 -0700

    task 12 adjusted 4

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,10 +5,10 @@
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
-
 void print_times_table(int n)
 {
 	int num, mult, prod;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (num = 0; num <= n; num++)
